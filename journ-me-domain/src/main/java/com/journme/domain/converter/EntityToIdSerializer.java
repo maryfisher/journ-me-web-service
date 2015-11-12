7afa2918c471cf2bfd333d8e88e75af35fc3245c
Hao Tri Pham <phamhaotri@googlemail.com>
2015-11-12 04:34:18 +0100

    MongoDB profiling - ensure that LazyLoadingProxies do not resolve to DB access, if simply serializing as ID string

--- journ-me-domain/src/main/java/com/journme/domain/converter/EntityToIdSerializer.java
+++ journ-me-domain/src/main/java/com/journme/domain/converter/EntityToIdSerializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.journme.domain.AbstractEntity;
+import org.springframework.data.mongodb.core.convert.LazyLoadingProxy;
 
 import java.io.IOException;
 
@@ -15,6 +16,10 @@ public class EntityToIdSerializer extends StdSerializer<AbstractEntity> {
 
     @Override
     public void serialize(AbstractEntity entity, JsonGenerator jgen, SerializerProvider provider) throws IOException {
-        jgen.writeString(entity.getId());
+        if (entity instanceof LazyLoadingProxy) {
+            jgen.writeString(((LazyLoadingProxy) entity).toDBRef().getId().toString());
+        } else {
+            jgen.writeString(entity.getId());
+        }
     }
 }
